4fe864c3f63687256452f6f86894e0518f130b41
Daniel Parks <dp+git@oxidized.org>
2019-07-22 22:43:13 -0700

    Output XML.

--- process.rb
+++ process.rb
@@ -11,14 +11,29 @@ def handle_index(path)
 
     title = html.at_css("#main h2.heading > a").content
     author = html.at_css("a[rel=author]").content
-    puts "“#{title}” by #{author}"
 
-    html.css("ol[role=navigation] > li").each do |chapter|
-      name = chapter.at_css("> a").content.split(" ", 2)[1]
-      date = chapter.at_css("> span.datetime").content
 
-      printf "  %-50s  %s\n", name, date
+    xml = Nokogiri::XML::Builder.new(:encoding => 'UTF-8')
+    xml.feed(:xmlns => "http://www.w3.org/2005/Atom") do
+      xml.title title
+
+      html.css("ol[role=navigation] > li").each do |chapter|
+        process_chapter xml, chapter
+      end
     end
+
+    puts xml.to_xml
+  end
+end
+
+def process_chapter(xml, chapter)
+  link = chapter.at_css("> a")
+  title = link.content.split(" ", 2)[1]
+  date = chapter.at_css("> span.datetime").content
+
+  xml.entry do
+    xml.title title
+    xml.link rel: "alternate", href: link[:href]
   end
 end
 
